a675277cdb063761a52d6529b230eace77da787c
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-03-07 21:41:45 +0000

    fix: container init scripts

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
-# shellcheck shell=bash disable=SC2034
+# shellcheck shell=bash
 
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 DEVCON_DIR="$(dirname "${SCRIPT_DIR}")"
 CONFIG_DIR="${DEVCON_DIR}/config"
 
+# Configure inputrc
+ln -sf "${CONFIG_DIR}/inputrc" "${HOME}/.inputrc"
+
+# .bashrc
 if [[ ! -f "${DEVCON_DIR}/.bashrc" ]]; then
     cp "${HOME}/.bashrc" "${DEVCON_DIR}/.bashrc"
     
     echo "export PROMPT_COMMAND='history -a'" >> "${DEVCON_DIR}/.bashrc"
     echo "export HISTFILE=${DEVCON_DIR}/.bash_history" >> "${DEVCON_DIR}/.bashrc"
+    
+    echo "alias ansible-playground='ansible -i /work/.config/hosts.playground.yml'"
+    echo "alias ansible-playground-playbook='ansible-playbook -i /work/.config/hosts.playground.yml'"
+    echo "alias ansible-pg='ansible -i /work/.config/hosts.playground.yml'"
+    echo "alias ansible-pp='ansible-playbook -i /work/.config/hosts.playground.yml'"
 fi
 ln -sf "${DEVCON_DIR}/.bashrc" "${HOME}/.bashrc"
 
