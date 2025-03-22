9961959718142459b4cdf65b59b4fbe7e4a10dd9
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-03-22 13:03:23 +0000

    feat: update container start script

--- .devcontainer/scripts/postStartCommand.sh
+++ .devcontainer/scripts/postStartCommand.sh
@@ -6,25 +6,43 @@ DEVCON_DIR="$(dirname "${SCRIPT_DIR}")"
 CONFIG_DIR="${DEVCON_DIR}/config"
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 
-echo "Configuring Git"
-# shellcheck disable=SC2154
-git config --global --add safe.directory /work
+printf "\n%s" "Configuring DevContainer..."
 
-# Configure filemode
-#git config core.filemode false
+# shellcheck disable=SC2154
+git -C "${PROJECT_DIR}" config --global --add safe.directory /work
+git -C "${PROJECT_DIR}" config core.autocrlf false
+git -C "${PROJECT_DIR}" config core.eol lf
+
+# Configure Ansible Vault Pass
+sudo chmod -x ${HOME}/.ansible_vault_pass
+
+# Configure ownership mounts
+sudo chmod 0600 "${HOME}/.ssh/config" 
+sudo chmod 0600 "${HOME}/.ssh/known_hosts"
+sudo chmod 0755 "${HOME}/.ssh/ansible"
+sudo chmod 0600 "${HOME}/.ssh/ansible/*.key"
+sudo chmod 0755 "${HOME}/.ssh/ansible/*.key.pub"
+sudo chown -R vscode:vscode "${HOME}/.ssh/ansible"
 
 if [[ ! -f "${DEVCON_DIR}/.bashrc" ]]; then
-    echo "Linking .bashrc"
     ln -sf "${DEVCON_DIR}/.bashrc" "${HOME}/.bashrc"
 fi
 
 if [ ! -f "${PROJECT_DIR}/hosts.yml" ]; then
-    echo "Installing Ansible Inventory Template"
     cp "${PROJECT_DIR}/.config/hosts.tmpl.yml" "${PROJECT_DIR}/hosts.yml"
 fi
 
-# Install collections
-ansible-galaxy collection install --force --collections-path .ansible/collections -r requirements.yml
-ansible-galaxy role install --force --roles-path .ansible/roles -r requirements.yml
+# Create Temp Directory
+if [[ ! -d /tmp/ansible ]]; then
+    mkdir -p /tmp/ansible
+fi
 
-echo "Container Start Completed"
+# Finalizing DevContainer
+printf "%s\n" "[DONE]"
+
+# Install collections
+printf "\n%s\n" "Running Ansible Galaxy"
+ANSIBLE_COLLECTIONS_PATH=/work/.ansible/collections ansible-galaxy collection install -U -p .ansible/collections -r requirements.yml
+ANSIBLE_ROLES_PATH=/work/.ansible/roles ansible-galaxy role install -p .ansible/roles -r requirements.yml
+
+printf "\n%s\n\n" "Container Start Completed"
